f7a7bc8e9a546185265f8c6f6af9959429a3fe31
santiago-gallinas <123750188+santiago-gallinas@users.noreply.github.com>
2023-04-06 12:12:30 +0200

    Quitando elementos innecesarios

--- WpfAppCronometro/App.xaml.cs
+++ WpfAppCronometro/App.xaml.cs
@@ -1,12 +1,4 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Configuration;
-using System.Data;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace WpfAppCronometro
@@ -36,19 +28,4 @@ namespace WpfAppCronometro
         }
     }
 }
-        //private void OnStartup(object sender, StartupEventArgs e)
-        //{
-        //    ServiceCollection services = new ServiceCollection();
-
-        //    services.AddSingleton<IClockController, ClockController>();
-        //    services.AddSingleton<IClockView, ClockView>();
-        //    services.AddSingleton<MainWindow>();
-
-        //    var serviceProvider = services.BuildServiceProvider();
-
-        //    //var mainWindow = serviceProvider.GetService<MainWindow>();
-        //    var mainWindow = services.BuildServiceProvider().GetService<MainWindow>();
-        //    mainWindow.Show();
-
-        //}
 
